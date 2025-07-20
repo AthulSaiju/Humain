20488e2cf8e8f6de036d8eb1a58ae91761b619f2
toughShit <athulsaiju02@gmail.com>
2025-07-20 16:36:01 +0530

    size adjustment

--- components/NavItems.tsx
+++ components/NavItems.tsx
@@ -14,7 +14,7 @@ const NavItems = () => {
     const pathname = usePathname();
 
     return (
-        <nav className="flex items-center gap-4">
+        <nav className="flex items-center gap-4 text-sm lg:text-lg">
             {navItems.map(({ label, href }) => (
                 <Link
                     href={href}
